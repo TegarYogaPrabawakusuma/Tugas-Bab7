fe86b6c3db1fbc6566150d7a69408357e1a3e6ad
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-05-11 19:51:54 +0700

    membuat class abstract dari salon

---
+++ src/tugas/bab7/AbsSalon.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktikum;
+
+/**
+ *
+ * @author TEGAR
+ */
+public abstract class AbsSalon {
+   public String Jasa;
+    public String Barang;
+    public abstract void jasa(String Jasa);
+    public abstract void barang(String Barang); 
+   public final int pedicure = 50000;
+    public final int manicure = 100000;
+    public final int BedakGatal = 25000;
+    public final int BedakJerawat = 50000; 
+   
+    
+}
