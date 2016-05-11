f0a77124d8a10194367a643d9f2a8988baa5cf68
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-05-11 21:41:26 +0700

    pembuatan main class part 1

---
+++ src/tugas/bab7/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktikum;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author TEGAR
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+        String Jasa, Barang;
+        System.out.println("Silahkan Memasukkan Jenis Keanggotaan: ");
+        System.out.println("1. Premium");
+        System.out.println("2. Golden");
+        System.out.println("3. Silver");
+        System.out.println("4. saya bukan anggota");
+        System.out.print("Pilih: ");
+        
+    }
+ }
