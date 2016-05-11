80f5c193ca540b61510484197e0b8021ed008aed
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-05-11 21:03:25 +0700

    class gold part 2(barang)

--- src/tugas/bab7/Gold.java
+++ src/tugas/bab7/Gold.java
@@ -30,4 +30,24 @@ public class Gold extends AbsSalon {
             System.out.println("Maaf, Layanan Yang Anda Pilih Tidak Tersedia");
         }
     } 
+    @Override
+    public void barang(String  Barang){
+        if (Barang.equalsIgnoreCase("produk krim untuk pemutih kulit")){
+            int total = KrimPutih - (KrimPutih * 10 / 100);
+            System.out.println("jenis pelayanan            : "+Barang);
+            System.out.println("Harga asli                 : Rp."+KrimPutih);
+            System.out.println("Discount untuk anggota     :  10%");
+            System.out.println("Total                      : "+total);
+        }
+        else if(Barang.equalsIgnoreCase("produk krim untuk mengobati jerawat")){
+            int total = KrimJerawat -(KrimJerawat * 10 / 100);
+            System.out.println("Layanan     : "+Barang);
+            System.out.println("Harga Awal  : Rp."+KrimJerawat);
+            System.out.println("Discount    :  10%");
+            System.out.println("Total       : Rp."+total);
+        }
+        else{
+            System.out.println("Produk Tidak Tersedia");
+        }
+    }   
 }
