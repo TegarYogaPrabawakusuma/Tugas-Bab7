fdd8860c6757ed02acaecc6e2999842fe5266cc6
Tegar Yoga Prabawakusuma <sirtegaryoga@gmail.com>
2016-05-11 21:42:54 +0700

    pembuatan main class part 2

--- src/tugas/bab7/Main.java
+++ src/tugas/bab7/Main.java
@@ -25,6 +25,48 @@ public class Main {
         System.out.println("3. Silver");
         System.out.println("4. saya bukan anggota");
         System.out.print("Pilih: ");
-        
+        int pilih = in.nextInt();
+        switch(pilih){
+            case 1 : System.out.print("Masukkan Jenis Jasa   : ");
+                     Jasa = in.next();
+                     System.out.print("Masukkan Jenis Barang : ");
+                     Barang = in.next();
+                     Premium a = new Premium();
+                     a.jasa(Jasa);
+                     System.out.println("/////=====/////");
+                     a.barang(Barang);
+                     break;
+            case 2 : System.out.print("Masukkan Layanan: ");
+                     System.out.print("Masukkan Jenis Jasa   : ");
+                     Jasa = in.next();
+                     System.out.print("Masukkan Jenis Barang : ");
+                     Barang = in.next();
+                     Gold b = new Gold();
+                     b.jasa(Jasa);
+                     System.out.println("/////=====/////");
+                     b.barang(Barang);
+                     break;
+            case 3 :System.out.print("Masukkan Layanan: ");
+                     System.out.print("Masukkan Jenis Jasa   : ");
+                     Jasa = in.next();
+                     System.out.print("Masukkan Jenis Barang : ");
+                     Barang = in.next();
+                     Silver c = new Silver();
+                     c.jasa(Jasa);
+                     System.out.println("/////=====/////");
+                     c.barang(Barang);
+                     break;
+            case 4 : System.out.print("Masukkan Layanan: ");
+                     System.out.print("Masukkan Jenis Jasa   : ");
+                     Jasa = in.next();
+                     System.out.print("Masukkan Jenis Barang : ");
+                     Barang = in.next();
+                     NoDiskon d = new NoDiskon();
+                     d.jasa(Jasa);
+                     System.out.println("/////=====/////");
+                     d.barang(Barang);
+                     break;
+            default : System.out.println("Pilihan Aanda Tidak Ada/Tidak Bisa Ditemukan");
+        }
     }
  }
